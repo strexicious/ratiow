4e86cbca87203e69226f6e9968c6d98ee8c4d056
Strexicious <strexicious@gmail.com>
2021-08-18 13:03:35 +0200

    Allow WriteColor to use `samples_per_pixel`

--- vec/color.go
+++ vec/color.go
@@ -23,7 +23,7 @@ func (c Color) B() float64 {
 	return c.e[2]
 }
 
-func (c Color) WriteColor(w io.Writer) {
-	c = c.Scale(255.999)
-	fmt.Fprintf(w, "%d %d %d\n", int(c.X()), int(c.Y()), int(c.Z()))
+func (c Color) WriteColor(w io.Writer, samples_per_pixel int32) {
+	c = c.Unscale(float64(samples_per_pixel)).ClampScalar(0, 0.999).Scale(256)
+	fmt.Fprintf(w, "%d %d %d\n", int(c.R()), int(c.G()), int(c.B()))
 }
