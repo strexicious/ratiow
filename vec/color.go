638a2d8f5b211ada8afcce366712bac4bbb5042a
Strexicious <strexicious@gmail.com>
2021-08-18 13:01:36 +0200

    Add R,G,B Color properties

--- vec/color.go
+++ vec/color.go
@@ -11,6 +11,18 @@ var ZeroColor = ZeroVec3
 
 var NewColor = NewVec3
 
+func (c Color) R() float64 {
+	return c.e[0]
+}
+
+func (c Color) G() float64 {
+	return c.e[1]
+}
+
+func (c Color) B() float64 {
+	return c.e[2]
+}
+
 func (c Color) WriteColor(w io.Writer) {
 	c = c.Scale(255.999)
 	fmt.Fprintf(w, "%d %d %d\n", int(c.X()), int(c.Y()), int(c.Z()))
